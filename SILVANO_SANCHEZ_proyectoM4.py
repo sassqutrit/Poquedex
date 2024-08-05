e263f23b91cdad60a0c8d7c50d8b6d4271de4445
sassqutrit <sass74050505@hotmail.com>
2024-08-05 14:30:37 -0600

    Agregación de comentarios

--- SILVANO_SANCHEZ_proyectoM4.py
+++ SILVANO_SANCHEZ_proyectoM4.py
@@ -3,14 +3,17 @@ import json
 import os
 
 def obtener_datos_pokemon(nombre):
+    # Hacer la petición a la API
     url = f"https://pokeapi.co/api/v2/pokemon/{nombre.lower()}"
     response = requests.get(url)
 
+    # Verificar el status code
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
+# Extraer la información relevante o mostrar los datos de un pokemon
 def mostrar_datos_pokemon(datos):
     print(f"Nombre: {datos['name'].capitalize()}")
     print(f"Peso: {datos['weight']} hectogramos")
@@ -20,14 +23,18 @@ def mostrar_datos_pokemon(datos):
     print("Movimientos: ", ", ".join([movimiento['move']['name'] for movimiento in datos['moves'][:5]]))  # Mostrando solo 5 movimientos
     print(f"Imagen: {datos['sprites']['front_default']}")
 
+# Guarda los datos de pokemon en un archivo .json que se encuentra dentro de una carpeta
 def guardar_datos_pokemon(datos):
+    # Crea la carpeta si esta no existe
     if not os.path.exists("pokedex"):
         os.makedirs("pokedex")
-    
+
+    # Guarda la información en un archivo .json
     nombre_archivo = f"pokedex/{datos['name']}.json"
     with open(nombre_archivo, 'w') as archivo:
         json.dump(datos, archivo, indent=4)
 
+# Función principal para introducir el nombre del pokemon y obtener sus datos
 def main():
     nombre_pokemon = input("Introduce el nombre del Pokémon: ")
     datos_pokemon = obtener_datos_pokemon(nombre_pokemon)
@@ -39,5 +46,6 @@ def main():
     else:
         print("Pokémon no encontrado. Por favor, verifica el nombre e inténtalo de nuevo.")
 
+# Se ejecuta la función principal si esta contiene ese mismo nombre
 if __name__ == "__main__":
     main()
